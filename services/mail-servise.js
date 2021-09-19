63523a9065d7d2cc34c0028921693d5ce030f02b
HellenWeb <84445465+HellenWeb@users.noreply.github.com>
2021-09-19 17:09:00 +1000

    Add changes in files and fixed bugs

--- services/mail-servise.js
+++ services/mail-servise.js
@@ -9,18 +9,30 @@ class MainServise {
 
     constructor() {
         this.trasport = nodemailer.createTransport({
-            host:
-            port: 
+            host: '' + process.env.SMTP_HOST,
+            port: '' + process.env.SMTP_PORT, 
             secure: false,
             auth: {
-                user:
-                pass: 
+                user: '' + process.env.SMTP_USER,
+                pass: '' + process.env.SMTP_PASSWORD
             }
         })
     }
 
     async sendActivationMain(to, link) {
-        
+        await this.trasport.sendMail({
+            from: '' + process.env.SMTP_USER,
+            to,
+            subject: `Активация аккаунта на ${'' + process.env.API_URL}`,
+            text: '',
+            html: 
+                `
+                    <div>
+                        <h1>Для активации перейдите по ссылке</h1>
+                        <a href='${link}'>${link}</a>
+                    </div>
+                `    
+        })
     }
 }
 
